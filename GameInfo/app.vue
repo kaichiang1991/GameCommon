90ce8b078e1117e39988d9611241f69675d63823
凱強 <kaichiang@starcrown.asia>
2021-09-01 03:03:27 +0000

    [GameInfo] 新增自動上到 GameCommon 的流程 需手動觸發

---
+++ GameInfo/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <div id="content">
+    </div>
+  </Layout>
+</template>
+
+<script>
+import mixin from '@/utils/mixin'
+
+export default {
+  name: '',
+  components: {
+  },
+  mixins: [
+    mixin,
+  ],
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
